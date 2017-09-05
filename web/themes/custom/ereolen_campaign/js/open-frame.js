0b650ee94b8fee2791c6206e42769510c9bcdfde
Martin Yde Granath <my@aarhus.dk>
2017-09-05 13:59:46 +0200

    EK-13: Changed player

---
+++ web/themes/custom/ereolen_campaign/js/open-frame.js
@@ -0,0 +1,17 @@
+(function ($, Drupal) {
+  Drupal.behaviors.ereolenCampaign = {
+    attach: function (context, settings) {
+      var iframe = $("#player");
+      $(".js-open-frame").click(function () {
+        var link_src = $(this).attr('data-src');
+        iframe.attr("src", link_src);
+        $(".js-frame").toggleClass('is-visible');
+      });
+
+      $(".js-frame-close").click(function () {
+        $(".js-frame").toggleClass('is-visible');
+        iframe.attr("src", "");
+      });
+    }
+  }
+})(jQuery, Drupal);
